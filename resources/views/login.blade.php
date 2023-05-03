81206bbfd5df1c0f5e11173985f70816b8d73989
alisson0101 <alisson.monteiro@escolar.ifrn.edu.br>
2023-05-03 10:24:56 -0300

    Algumas alterações

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,6 @@
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,7 +8,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Bem vindos</h1>
+    <h1>OLÁ</h1>
 </body>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -17,7 +17,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="app.css">
+    <link rel="stylesheet" href="/public/css/estilo-login.css">
     <link rel="stylesheet" href="bootstrap-5.0.2-dist/css/bootstrap-reboot.min.css">
     <title>Login</title>
 </head>
